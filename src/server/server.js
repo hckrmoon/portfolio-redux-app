3959332e95fdf1a0c31a4e39e2f0621e9488c535
hckrmoon <hckrmoon@gmail.com>
2016-02-07 14:02:05 +0900

    Update server.js
    I think arrow function is okay to be applied here.

--- src/server/server.js
+++ src/server/server.js
@@ -44,7 +44,7 @@ if(process.env.NODE_ENV !== 'production'){
   app.use('/static', express.static(__dirname + '/../../dist'));
 }
 
-app.get('/*', function (req, res) {
+app.get('/*', (req, res) => {
 
   const location = createLocation(req.url);
 
@@ -84,7 +84,7 @@ app.get('/*', function (req, res) {
 
 });
 
-const server = app.listen(3002, function () {
+const server = app.listen(3002, () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
